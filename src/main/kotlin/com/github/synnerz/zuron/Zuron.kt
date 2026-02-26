632e0e32acbc8ac2fc09fb4e2941d9642164d265
Doc <65479309+DocilElm@users.noreply.github.com>
2026-02-25 18:48:06 -0700

    initial commit

---
+++ src/main/kotlin/com/github/synnerz/zuron/Zuron.kt
@@ -0,0 +1,11 @@
+package com.github.synnerz.zuron
+
+import net.fabricmc.api.ClientModInitializer
+import org.slf4j.LoggerFactory
+
+object Zuron : ClientModInitializer {
+    private val logger = LoggerFactory.getLogger("zuron")
+
+	override fun onInitializeClient() {
+	}
+}
