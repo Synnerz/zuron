57194186aca34c2548f6a1106cdf2b86c0b933a0
Doc <65479309+DocilElm@users.noreply.github.com>
2026-03-01 18:11:19 -0700

    Register: fix not working for every engine

--- src/main/kotlin/com/github/synnerz/zuron/internal/Register.kt
+++ src/main/kotlin/com/github/synnerz/zuron/internal/Register.kt
@@ -1,5 +1,14 @@
 package com.github.synnerz.zuron.internal
 
+import com.github.synnerz.zuron.js.JSLoader
+import com.github.synnerz.zuron.js.JSLoader.wrapInContext
+import org.luaj.vm2.LuaClosure
+import org.luaj.vm2.LuaValue
+import org.luaj.vm2.lib.jse.CoerceJavaToLua
+import org.mozilla.javascript.Callable
+import org.mozilla.javascript.Context
+import org.python.core.Py
+import org.python.core.PyObject
 import java.util.concurrent.ConcurrentHashMap
 
 /**
@@ -7,17 +16,28 @@ import java.util.concurrent.ConcurrentHashMap
  * [Link](https://github.com/ChatTriggers/ctjs/blob/main/src/main/kotlin/com/chattriggers/ctjs/engine/Register.kt)
  */
 object Register {
-    val customRegisters = ConcurrentHashMap<String, (Array<Any>) -> Unit>()
-    val regs = ConcurrentHashMap<String, MutableList<(Array<out Any?>) -> Unit>>()
+    val customRegisters = ConcurrentHashMap<String, Any>()
+    val regs = ConcurrentHashMap<String, MutableList<Any>>()
 
     @JvmStatic
-    fun register(name: String, method: (Array<out Any?>) -> Unit) {
-        val type = name.lowercase()
+    fun register(name: String, method: Any) {
+        val type = name.lowercase().trim()
         regs.getOrPut(type, { mutableListOf() }).add(method)
     }
 
     @JvmStatic
     fun trigger(name: String, vararg args: Any?) {
-        regs.forEach { it.value.forEach { it.invoke(args) } }
+        val type = name.lowercase().trim()
+        val methods = regs[type] ?: return
+        methods.forEach { method ->
+            when (method) {
+                is Callable -> wrapInContext {
+                    Context.jsToJava(method.call(it, JSLoader.moduleScope, JSLoader.moduleScope, args), Any::class.java)
+                }
+                is LuaClosure -> if (method.isfunction()) method.invoke(LuaValue.varargsOf(args.map { CoerceJavaToLua.coerce(it) }.toTypedArray()))
+                is PyObject -> if (method.isCallable) method.__call__(args.map { Py.java2py(it) }.toTypedArray())
+
+            }
+        }
     }
 }
